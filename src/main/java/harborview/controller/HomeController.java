d39bbec4d8855f3a83c682d24f3f6970a92a2199
hamilton108 <hamilton108@palenque.com>
2018-04-26 07:57:27 +0200

    Bare-bones controller

--- src/main/java/harborview/controller/HomeController.java
+++ src/main/java/harborview/controller/HomeController.java
@@ -1,4 +1,15 @@
 package harborview.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
+    @RequestMapping(method =  RequestMethod.GET)
+    public String sayHello(Model model) {
+        return "Hello";
+    }
 }
