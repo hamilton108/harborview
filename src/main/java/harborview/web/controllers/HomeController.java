854674dcc1a632c04e4caa795bc9d02b0a736aeb
hamilton108 <hamilton108@palenque.com>
2018-04-27 08:02:11 +0200

    Refactored file names / directory structure

--- src/main/java/harborview/web/controllers/HomeController.java
+++ src/main/java/harborview/web/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package harborview.controller;
+package harborview.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
