27b2f1caa6f52f446919ea4eb491b37e5794f76b
hamilton108 <hamilton108@palenque.com>
2018-04-28 19:26:56 +0200

    Spring MVC config success

--- src/main/java/harborview/web/controllers/HomeController.java
+++ src/main/java/harborview/web/controllers/HomeController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
 
+import java.util.Locale;
+
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
     @RequestMapping(method =  RequestMethod.GET)
-    public String sayHello(Model model) {
-        return "Hello";
+    public String index(Locale locale, Model model) {
+        System.out.printf("here I am");
+        return "home";
     }
 }
