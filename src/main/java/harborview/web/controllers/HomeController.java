3fb3067fd679568f5ab508ae85f8c2577db3f2be
hamilton108 <hamilton108@palenque.com>
2018-05-30 10:15:03 +0200

    StockMarketRepos bean autowired in controller

--- src/main/java/harborview/web/controllers/HomeController.java
+++ src/main/java/harborview/web/controllers/HomeController.java
@@ -1,14 +1,13 @@
 package harborview.web.controllers;
 
-import oahu.financial.Derivative;
 import oahu.financial.Stock;
-import oahu.financial.StockPrice;
+import oahu.financial.repository.StockMarketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -16,53 +15,16 @@ import java.util.Locale;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private final StockMarketRepository stockMarketRepository;
+
+    @Autowired
+    HomeController(StockMarketRepository stockMarketRepository) {
+        this.stockMarketRepository = stockMarketRepository;
+    }
     @RequestMapping(method =  RequestMethod.GET)
     public String index(Locale locale, Model model) {
-        model.addAttribute("stockTickers", getStocks());
+        model.addAttribute("stockTickers", stockMarketRepository.getStocks());
         return "index.html";
-    }
-    private List<Stock> getStocks() {
-        List<Stock> result = new ArrayList<>();
-        result.add(new StockDemo("NHY", 1));
-        result.add(new StockDemo("YAR", 2));
-        result.add(new StockDemo("STL", 3));
-        return result;
-    }
-}
-class StockDemo implements Stock {
-    private final String ticker;
-    private final int oid;
-    public StockDemo(String ticker, int oid) {
-        this.ticker = ticker;
-        this.oid = oid;
-    }
-    @Override
-    public String getCompanyName() {
-        return null;
-    }
-
-    @Override
-    public String getTicker() {
-        return ticker;
-    }
-
-    @Override
-    public int getTickerCategory() {
-        return 0;
-    }
-
-    @Override
-    public int getOid() {
-        return oid;
-    }
-
-    @Override
-    public List<StockPrice> getPrices() {
-        return null;
-    }
-
-    @Override
-    public List<Derivative> getDerivatives() {
-        return null;
     }
 }
--- src/main/java/harborview/web/controllers/MaunaloaController.java
+++ src/main/java/harborview/web/controllers/MaunaloaController.java
@@ -1,10 +1,12 @@
 package harborview.web.controllers;
 
+import oahu.financial.Stock;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 @Controller
@@ -13,6 +15,7 @@ public class MaunaloaController {
 
     @RequestMapping(value = "charts", method =  RequestMethod.GET)
     public String index(Locale locale, Model model) {
+        model.addAttribute("stockTickers", new ArrayList<Stock>());
         return "maunaloa/charts.html";
     }
 
