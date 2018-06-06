1170c731d204c17d01d9874eae3c800f2518ccbb
hamilton108 <hamilton108@palenque.com>
2018-06-06 21:10:17 +0200

    Css grid for charts

--- src/main/webapp/js/maunaloa/charts.js
+++ src/main/webapp/js/maunaloa/charts.js
@@ -1,15 +1,15 @@
 var MAUNALOA = MAUNALOA || {};
 
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  var repos1 = null;
-  var repos2 = null;
-  var repos3 = null;
-  var node = document.getElementById('my-app');
-  var app = Elm.Maunaloa.Charts.embed(node, {
+document.addEventListener("DOMContentLoaded", function() {
+  let repos1 = null;
+  let repos2 = null;
+  let repos3 = null;
+  const node = document.getElementById('my-app');
+  const app = Elm.Maunaloa.Charts.embed(node, {
     chartResolution: 1
   });
-  var scrapbook1 = MAUNALOA.scrapbook.create({
+  const scrapbook1 = MAUNALOA.scrapbook.create({
     id_checkbox: "scrapbook1",
     id_div1: "div-1x",
     id_divScrap: "div-1scrap",
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     ],
     id_rgLine: "rg-line1"
   });
-  var scrapbook2 = MAUNALOA.scrapbook.create({
+  const scrapbook2 = MAUNALOA.scrapbook.create({
     id_checkbox: "scrapbook2",
     id_div1: "div-2x",
     id_divScrap: "div-2scrap",
@@ -48,40 +48,40 @@ document.addEventListener("DOMContentLoaded", function(event) {
     id_rgLine: "rg-line2"
   });
 
-  var node2 = document.getElementById('my-app2');
-  var app2 = Elm.Maunaloa.Charts.embed(node2, {
+  /*
+  const node2 = document.getElementById('my-app2');
+  const app2 = Elm.Maunaloa.Charts.embed(node2, {
     chartResolution: 2
   });
 
-  var node3 = document.getElementById('my-app3');
-  var app3 = Elm.Maunaloa.Charts.embed(node3, {
+  const node3 = document.getElementById('my-app3');
+  const app3 = Elm.Maunaloa.Charts.embed(node3, {
     chartResolution: 3
   });
-  <!------------- canvas sizes ---------------->
-  var setCanvasSize = function(selector, w, h) {
-    var c1 = document.querySelectorAll(selector);
-    for (var i = 0; i < c1.length; ++i) {
-      var canvas = c1[i];
+  */
+  const setCanvasSize = function(selector, w, h) {
+    const c1 = document.querySelectorAll(selector);
+    for (let i = 0; i < c1.length; ++i) {
+      const canvas = c1[i];
       canvas.width = w;
       canvas.height = h;
     }
-  }
-  var setCanvasSizes = function() {
+  };
+  const setCanvasSizes = function() {
     setCanvasSize('canvas.c1', 1310, 500);
     setCanvasSize('canvas.c2', 1310, 200);
     setCanvasSize('canvas.c3', 1310, 110);
-  }
+  };
   setCanvasSizes();
-  <!------------- drawCanvas ---------------->
-  var drawCanvas1 = function(chartInfo) {
-    var cfg = {
+  const drawCanvas1 = function(chartInfo) {
+    const cfg = {
       ci: chartInfo,
       ch: chartInfo.chart,
       cid: MAUNALOA.repos.DAY_LINES,
       cidx: MAUNALOA.repos.DAY_LINES_OVERLAY,
       reposId: 1,
       isMain: true
-    }
+    };
     drawCanvas(cfg);
     cfg.isMain = false;
     if (chartInfo.chart2 != null) {
@@ -95,16 +95,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
       drawCanvas(cfg);
     }
     scrapbook1.clearCanvas();
-  }
-  var drawCanvas2 = function(chartInfo) {
-    var cfg = {
+  };
+  const drawCanvas2 = function(chartInfo) {
+    const cfg = {
       ci: chartInfo,
       ch: chartInfo.chart,
       cid: MAUNALOA.repos.WEEK_LINES,
       cidx: MAUNALOA.repos.WEEK_LINES_OVERLAY,
       reposId: 2,
       isMain: true
-    }
+    };
     drawCanvas(cfg);
     cfg.isMain = false;
     if (chartInfo.chart2 != null) {
@@ -118,39 +118,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
       drawCanvas(cfg);
     }
     scrapbook2.clearCanvas();
-  }
-  var drawCanvas3 = function(chartInfo) {
-    var cfg = {
+  };
+  const drawCanvas3 = function(chartInfo) {
+    const cfg = {
       ci: chartInfo,
       ch: chartInfo.chart,
       cid: MAUNALOA.repos.MONTH_LINES,
       cidx: MAUNALOA.repos.MONTH_LINES_OVERLAY,
       reposId: 3,
       isMain: true
-    }
+    };
     drawCanvas(cfg);
-  }
+  };
   app.ports.drawCanvas.subscribe(drawCanvas1);
-  app2.ports.drawCanvas.subscribe(drawCanvas2);
-  app3.ports.drawCanvas.subscribe(drawCanvas3);
+  //app2.ports.drawCanvas.subscribe(drawCanvas2);
+  //app3.ports.drawCanvas.subscribe(drawCanvas3);
 
-  var clearCanvas = function(canvasId) {
-    var canvas = document.getElementById(canvasId);
-    var ctx = canvas.getContext("2d");
+  const clearCanvas = function(canvasId) {
+    const canvas = document.getElementById(canvasId);
+    const ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     return [ctx, canvas];
-  }
-  var drawCanvas = function(cfg) {
-    var chartInfo = cfg.ci;
-    var curChart = cfg.ch;
-    var offsets = chartInfo.xaxis;
-    var ctx, canvas;
+  };
+  const drawCanvas = function(cfg) {
+    const chartInfo = cfg.ci;
+    const curChart = cfg.ch;
+    const offsets = chartInfo.xaxis;
+    let ctx, canvas;
     [ctx, canvas] = clearCanvas(cfg.cid);
 
-    var myHruler = MAUNALOA.hruler(1300, chartInfo.startdate, offsets, curChart.bars === null, 5);
+    const myHruler = MAUNALOA.hruler(1300, chartInfo.startdate, offsets, curChart.bars === null, 5);
     myHruler.lines(ctx, canvas.height, chartInfo.numIncMonths);
 
-    var myVruler = MAUNALOA.vruler(canvas.height, curChart.valueRange);
+    const myVruler = MAUNALOA.vruler(canvas.height, curChart.valueRange);
     myVruler.lines(ctx, canvas.width, curChart.numVlines);
 
     if (cfg.isMain === true) {
@@ -172,17 +172,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
     }
 
-    var lineChart = MAUNALOA.lineChart(myHruler, myVruler, ctx);
-    var strokes = chartInfo.strokes;
+    const lineChart = MAUNALOA.lineChart(myHruler, myVruler, ctx);
+    const strokes = chartInfo.strokes;
     if (curChart.lines !== null) {
-      for (var i = 0; i < curChart.lines.length; ++i) {
-        var line = curChart.lines[i];
-        var curStroke = strokes[i] === undefined ? "#000000" : strokes[i];
+      for (let i = 0; i < curChart.lines.length; ++i) {
+        const line = curChart.lines[i];
+        const curStroke = strokes[i] === undefined ? "#000000" : strokes[i];
         lineChart.drawLine(line, curStroke);
       }
     }
     if (curChart.bars !== null) {
-      for (var i = 0; i < curChart.bars.length; ++i) {
+      for (let i = 0; i < curChart.bars.length; ++i) {
         lineChart.drawBars(curChart.bars[i]);
       }
     }
@@ -190,49 +190,53 @@ document.addEventListener("DOMContentLoaded", function(event) {
     if (curChart.candlesticks !== null) {
       lineChart.drawCandlesticks(curChart.candlesticks);
     }
-  }
+  };
 
-  <!------------- Level line ---------------->
-  var levelLineBtn1 = document.getElementById("btn-levelline-1");
+  //------------- Level line ---------------->
+    /*
+  const levelLineBtn1 = document.getElementById("btn-levelline-1");
   levelLineBtn1.onclick = function() {
     repos1.addLevelLine(100, true);
   };
-  var levelLineBtn2 = document.getElementById("btn-levelline-2");
+  const levelLineBtn2 = document.getElementById("btn-levelline-2");
   levelLineBtn2.onclick = function() {
     repos2.addLevelLine(100, true);
   };
-  <!------------- drawRiscLines ---------------->
+  */
+  //------------- drawRiscLines ---------------->
 
-  var drawRiscLines1 = function(riscLinesInfo) {
+  const drawRiscLines1 = function(riscLinesInfo) {
     drawRiscLines(riscLinesInfo, 1);
-  }
-  var drawRiscLines2 = function(riscLinesInfo) {
+  };
+  const drawRiscLines2 = function(riscLinesInfo) {
     drawRiscLines(riscLinesInfo, 2);
-  }
-  var drawRiscLines = function(riscLinesInfo, reposId) {
-    var repos = reposId === 1 ? repos1 : repos2;
+  };
+  const drawRiscLines = function(riscLinesInfo, reposId) {
+    const repos = reposId === 1 ? repos1 : repos2;
     if (repos === null) {
-      alert("Repos not initialized! Aborting.")
+      alert("Repos not initialized! Aborting.");
       return;
     }
     repos.reset();
-    var riscLines = riscLinesInfo.riscLines;
-    for (var i = 0; i < riscLines.length - 1; ++i) {
-      var rl = riscLines[i];
+    const riscLines = riscLinesInfo.riscLines;
+    for (let i = 0; i < riscLines.length - 1; ++i) {
+      const rl = riscLines[i];
       repos.addRiscLines(rl, false);
     }
     if (riscLines.length > 0) {
-      var rlLast = riscLines[riscLines.length - 1];
+      const rlLast = riscLines[riscLines.length - 1];
       repos.addRiscLines(rlLast, true);
     }
-  }
+  };
   /*
   app.ports.drawRiscLines.subscribe(drawRiscLines1);
   app2.ports.drawRiscLines.subscribe(drawRiscLines2);
   */
 
-  var drawSpot1 = function(spot) {
+  /*
+  const drawSpot1 = function(spot) {
     repos1.addSpot(spot);
-  }
+  };
   app.ports.drawSpot.subscribe(drawSpot1);
+  */
 });
