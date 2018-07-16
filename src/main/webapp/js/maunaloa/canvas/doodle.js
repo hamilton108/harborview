0839cc485ad5ffd46116deb226479c552025d240
hamilton108 <hamilton108@palenque.com>
2018-07-16 14:58:47 +0200

    Line impl

--- src/main/webapp/js/maunaloa/canvas/doodle.js
+++ src/main/webapp/js/maunaloa/canvas/doodle.js
@@ -1,9 +1,9 @@
 
 export class Doodle {
     constructor(cfg) {
-        const canvas = document.getElementById(cfg.DOODLE);
-        if (canvas !== null) {
-            canvas.addEventListener('mousedown', this.handleMouseDown.bind(this), false);
+        this.canvas = document.getElementById(cfg.DOODLE);
+        if (this.canvas !== null) {
+            this.canvas.addEventListener('mousedown', this.handleMouseDown.bind(this), false);
             /*
             c_scrap.addEventListener('mousemove', this.handleMouseMove(this), false);
             c_scrap.addEventListener('mouseup', this.handleMouseDone(this), false);
@@ -17,6 +17,11 @@ export class Doodle {
         this.guiEvent(cfg.BTN_HORIZ, (e) => { this.MODE = Doodle.MODE_HORIZ_BEGIN; });
         this.guiEvent(cfg.BTN_ARROW, (e) => { this.MODE = Doodle.MODE_ARROW; });
         this.getLineSize = this.getLineSizeFn(cfg.RG_LINE_SIZE);
+
+        this.color = document.getElementById(cfg.COLOR);
+    }
+    get curContext() {
+        return this.canvas.getContext("2d");
     }
     guiEvent(domId,fn) {
         const obj = document.getElementById(domId);
@@ -48,9 +53,15 @@ export class Doodle {
                 this.MODE = Doodle.MODE_LINE_END;
                 break;
             case Doodle.MODE_LINE_END:
+                const ctx = this.curContext;
+                ctx.lineWidth = this.getLineSize();
+                ctx.strokeStyle = this.color.value;
+                ctx.beginPath();
+                ctx.moveTo(this.p0.x, this.p0.y);
+                ctx.lineTo(e.offsetX, e.offsetY);
+                ctx.stroke();
+                this.p0 = null;
                 this.MODE = Doodle.MODE_NONE;
-                console.log(this.p0);
-                console.log(e);
                 break;
             case Doodle.MODE_HORIZ_BEGIN:
                 this.MODE = Doodle.MODE_HORIZ_END;
