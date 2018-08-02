9ddd962eda96dc19415050ca0ab7c3b1d9cec0cc
hamilton108 <hamilton108@palenque.com>
2018-08-02 23:02:46 +0200

    Critters navbar, controller, overlook skeleton

--- src/main/java/harborview/web/controllers/CritterController.java
+++ src/main/java/harborview/web/controllers/CritterController.java
@@ -1,4 +1,18 @@
 package harborview.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/critters")
 public class CritterController {
+
+    @RequestMapping(value = "overlook/{ct}", method =  RequestMethod.GET)
+    public String critters(@PathVariable("ct") int critterType, Model model) {
+        model.addAttribute("demo", "demo");
+        return "critters/overlook.html";
+    }
 }
