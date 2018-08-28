ed7cfe3a36a6ee762a28140589f1256204fc7051
hamilton108 <hamilton108@palenque.com>
2018-08-28 23:05:37 +0200

    Purchase area critter row first stab

--- elm/src/Critters/Main.elm
+++ elm/src/Critters/Main.elm
@@ -3,7 +3,7 @@ module Critters.Main exposing (..)
 import Html as H
 import Critters.Update as U
 import Critters.Views as V
-import Critters.Types exposing (Model, Flags, Msg(..))
+import Critters.Types as T exposing (Model, Flags, Msg(..))
 import Common.ComboBox as CB
 
 
@@ -24,4 +24,21 @@ init flags =
 
 initModel : Flags -> Model
 initModel flags =
-    {}
+    let
+        dny1 =
+            T.DenyRule 1 1 2.0 True False
+
+        dny2 =
+            T.DenyRule 2 2 4.0 True True
+
+        acc =
+            T.AccRule 1 7 5.5 True (Just [ dny1, dny2 ])
+
+        critter =
+            T.Critter 1 10 1 (Just [ acc ])
+
+        opx =
+            T.OptionPurchase 1 "YAR8L240" (Just [ critter ])
+    in
+        { purchases = Just [ opx ]
+        }
--- elm/src/Critters/Types.elm
+++ elm/src/Critters/Types.elm
@@ -3,10 +3,78 @@ module Critters.Types exposing (..)
 import Common.ComboBox as CB
 
 
+rtypDesc : Int -> String
+rtypDesc rtyp =
+    case rtyp of
+        1 ->
+            "Diff from watermark"
+
+        2 ->
+            "Diff from watermark percent"
+
+        3 ->
+            "Stock price floor (valid if above price)"
+
+        4 ->
+            "Stock price roof (valid if below price)"
+
+        5 ->
+            "Option price floor (valid if above price)"
+
+        6 ->
+            "Option price roof (valid if below price)"
+
+        7 ->
+            "Diff from bought"
+
+        9 ->
+            "Gradient diff from watermark"
+
+        _ ->
+            "Composite"
+
+
 type Msg
     = PaperCritters
     | RealTimeCritters
     | NewCritter
+
+
+type alias OptionPurchase =
+    { oid : Int
+    , ticker : String
+    , critters : Maybe (List Critter)
+    }
+
+
+type alias OptionPurchases =
+    List OptionPurchase
+
+
+type alias Critter =
+    { oid : Int
+    , sellVolume : Int
+    , status : Int
+    , accRules : Maybe (List AccRule)
+    }
+
+
+type alias AccRule =
+    { oid : Int
+    , rtyp : Int
+    , value : Float
+    , active : Bool
+    , denyRules : Maybe (List DenyRule)
+    }
+
+
+type alias DenyRule =
+    { oid : Int
+    , rtyp : Int
+    , value : Float
+    , active : Bool
+    , memory : Bool
+    }
 
 
 type alias Flags =
@@ -14,4 +82,5 @@ type alias Flags =
 
 
 type alias Model =
-    {}
+    { purchases : Maybe OptionPurchases
+    }
