43ec599e6cb55e54ec559429ffd9ef558729c015
hamilton108 <hamilton108@palenque.com>
2018-11-28 00:52:20 +0100

    Testing parsing derivatives

--- local/session.rb
+++ local/session.rb
@@ -19,6 +19,8 @@ def init
 
   $dl = $ctx.getBean "downloader"
 
+  $etrade = $ctx.getBean "etrade"
+
   $f = File.new "/home/rcs/opt/haskell/etradejanitor/feed/2018/11/23/NHY.html"
 
   $doc = Jsoup.parse($f, "UTF-8")
@@ -26,7 +28,6 @@ def init
   $el = $doc.getElementsByClass "com topmargin"
 end
 
-#init
 
 def optionName(tds)
   myEl = tds.get 0
@@ -38,32 +39,77 @@ def optionType(tds)
   myEl = tds.get 1
   myEl.text
 end
-
 def exercise(tds)
   myEl = tds.get 2
   myEl.text
+end
+def expiry(tds)
+  myEl = tds.get 3
+  "expiry: " + myEl.text
+end
+def buy(tds)
+  myEl = tds.get 4
+  "Buy: " + myEl.text
+end
+def sell(tds)
+  myEl = tds.get 5
+  "Sell: " + myEl.text
 end
 
 def processOption(el)
   tds = el.children
   puts optionName tds
   puts optionType tds
+  puts exercise tds
+  puts expiry tds
+  puts buy tds
+  puts sell tds
 end
 
 $filterLamb = ->(v) {
+    puts v
     optionName v == "NHY8L30"
 }
 
 def run
   f = $el.first
+  puts f.getClass.getName
   fx = f.getElementsByTag "tr"
-  fxx = fx.get(10)
+  fxx = fx.get(3)
   processOption fxx
 end
 
-def run2
-  stream = $el.stream()
-  l1 = ->() { java.util.ArrayList.new }
-  result = stream.filter($filterLamb).collect(l1)
-  puts result
+def checkNode(tr)
+  sz = tr.childNodeSize
+  if sz > 15
+    ch = tr.child(9).text
+    puts "#{ch} #{sz}"
+  end
 end
+
+def run2
+  f = $el.first
+  fx = f.getElementsByTag "tr"
+  # puts fx.get(4).child(1)
+  fx.each {|td| checkNode td }
+end
+
+def testNode
+  f = $el.first
+  fx = f.getElementsByTag "tr"
+  fx.get(4)
+end
+
+def run3
+  f = $el.first
+  fx = f.getElementsContainingOwnText "American call"
+  fx.each {|x|
+    cz = x.parent.childNodeSize
+    if cz > 15
+      buy = x.parent.child(4).text
+      sell = x.parent.child(5).text
+      puts "Buy #{buy}, sell #{sell}"
+    end}
+end
+
+init
