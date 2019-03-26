82c8e9c167c5f30875e81b834217636ed92bff9a
Hamilton 108 <hamilton@hamilton.com>
2019-03-26 22:53:19 +0100

    CommandLineRunner

--- src/main/java/harborview/App.java
+++ src/main/java/harborview/App.java
@@ -1,11 +1,20 @@
 package harborview;
 
+import critterrepos.mybatis.StockMapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+@MapperScan("critterrepos.mybatis")
+public class App implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 }
