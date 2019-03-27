707ffcd49b0675d0b1c35328e149e436131305ee
Hamilton 108 <hamilton@hamilton.com>
2019-03-27 23:31:49 +0100

    MyBatis demo

--- src/main/java/harborview/App.java
+++ src/main/java/harborview/App.java
@@ -1,20 +1,37 @@
 package harborview;
 
-import critterrepos.mybatis.StockMapper;
+import oahu.financial.Stock;
+import oahu.financial.repository.StockMarketRepository;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Collection;
 
 @SpringBootApplication
-@MapperScan("critterrepos.mybatis")
+@MapperScan("critterrepos.models.mybatis")
+@ComponentScan({"oahu.financial.repository","critterrepos.models.impl"})
 public class App implements CommandLineRunner {
+
+    StockMarketRepository repository;
+
+    @Autowired
+    public App(StockMarketRepository repository) {
+        this.repository = repository;
+    }
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        System.out.printf("My repository: " + repository);
+        Collection<Stock> stox = repository.getStocks();
+        for (Stock s : stox) {
+            System.out.println(s.getTicker());
+        }
     }
 }
