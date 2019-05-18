764292207612d8911f3b3574b61f531b6d35c7ac
Hamilton 108 <hamilton@hamilton.com>
2019-05-18 09:55:49 +0200

    hilo, .gitignore for Purescript

--- src/main/java/harborview/service/config/DefaultConfig.java
+++ src/main/java/harborview/service/config/DefaultConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 import java.io.Serializable;
 
 @Configuration
-@Profile("default")
+@Profile({"default","hilo"})
 public class DefaultConfig {
 
     @Bean
