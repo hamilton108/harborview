d546addf223217f3693bf20df8ab39ce931ac3c2
Hamilton 108 <hamilton@hamilton.com>
2019-11-04 23:08:54 +0100

    Add line 2 (button click) demo

--- purescript/src/Maunaloa/LevelLine.js
+++ purescript/src/Maunaloa/LevelLine.js
@@ -13,6 +13,26 @@ exports.createLine = function (vruler) {
     };
 };
 
+exports.createLine2 = function (ctx) {
+    return function (vruler) {
+        return function () {
+            console.log(ctx);
+            console.log(vruler);
+            /*
+            const y = vruler.h / 2.0;
+            const x0 = 25.0;
+            const x1 = vruler.w - x0;
+            ctx.lineWidth = 2.5;
+            ctx.beginPath();
+            ctx.moveTo(x0, y);
+            ctx.lineTo(x1, y);
+            ctx.stroke();
+            */
+            return { y: 20.0, draggable: true };
+        };
+    };
+};
+
 const pixToValue = function (v, pix) {
     return v.maxVal - ((pix - v.padding.top) / v.ppy);
 };
