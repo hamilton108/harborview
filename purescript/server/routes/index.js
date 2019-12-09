0035c51df7114c54c82a880063261298ddea11f7
Hamilton 108 <hamilton@hamilton.com>
2019-12-09 10:28:15 +0100

    Modified index to level line demo

--- purescript/server/routes/index.js
+++ purescript/server/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: "2D Graphics Demo" });
 });
 
+router.get('/rox', function (req, res, next) {
+  res.json({ a: "Fox", b: { c: "dux" } });
+});
 module.exports = router;
